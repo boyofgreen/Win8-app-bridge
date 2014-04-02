c1490444639c8b31e0915001c0eef7cd95f0226b
boyofgreen <boyofgreen@gmail.com>
2014-04-01 23:11:39 -0700

    initial commit

---
+++ Win8-hosted-bridge/cordova-bridge.Shared/js/test.js
@@ -0,0 +1,30 @@
+﻿
+
+
+document.addEventListener("deviceready", function (e) {
+    console.log('device ready %%%%%%%%%%%%')
+    document.getElementById('deviceState').textContent = "DEVICE READY";
+
+    //test the geolocation API
+    
+    navigator.geolocation.getCurrentPosition(function (e) {
+        
+        document.getElementById('geoState').textContent = 'GEO NOW READY= ' + e.coords.longitude;
+
+    }, function (e) { console.log('error') });
+     
+
+    //test the notifications
+
+    navigator.notification.confirm('winner',
+        function () { document.getElementById('notiicationState').textContent = 'Notification READY' })
+
+
+
+
+
+
+
+}, false);
+
+
---
+++ Win8-hosted-bridge/cordova-bridge.Shared/js/win8-bridge-clinet.js
@@ -0,0 +1,35 @@
+﻿var WV = document.getElementById('webView');
+
+
+//setting the device ready state in the webveiw
+document.addEventListener("deviceready", function (e) {
+    var eval = WV.invokeScriptAsync('eval', 'deviceReady()');
+
+    setTimeout(function () { eval.start() }, 1500);
+}, false);
+
+
+
+
+//setting up the receiver for GEo events
+WV.addEventListener('MSWebViewFrameNavigationStarting', function (e) {
+    var eventName = e.uri.split('EVENT=');
+    if (eventName = 'geoEvent') {
+
+        //make call to real geo
+        navigator.geolocation.getCurrentPosition(function (g) {
+            returnEvent(g);
+        }, function (g) { console.log('error') });
+        //inject results into webview
+        var returnEvent = function (g) {
+            var evalString = 'var geoobj = {}; geoobj.coords = {} ;geoobj.coords.longitude = ' + g.coords.longitude + '; geoobj.coords.latitude = ' + g.coords.latitude + '; geoReady(geoobj)';
+            var eval = WV.invokeScriptAsync('eval', evalString);
+            eval.start();
+
+        };
+    }
+
+
+
+});
+
